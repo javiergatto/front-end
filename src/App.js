aceaf5866a84571ab82e6cc72c697bdfb27b6830
first repo <xhavier4@hotmail.com>
2021-03-08 20:46:29 -0500

    adding

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        < low on funds , send on only fands account !</p>
+        < low on funds send on only fands account !</p>
         <a
           className="App-link"
           href="https://reactjs.org"
